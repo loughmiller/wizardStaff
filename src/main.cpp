3cdc3162b73df8b411fd9a160caf19f9a39902e1
Scott Loughmiller <scott@scalecomputing.com>
2017-06-16 01:38:52 -0700

    all together now

--- src/main.cpp
+++ src/main.cpp
@@ -15,13 +15,18 @@ CRGB off;
 void clear();
 void setAll();
 
-#define NUM_STREAKS 4
+#define NUM_STREAKS 12
+#define NUM_LADDERS 4
 
 CRGB pink = 0x220102;
-//Streak * streaks[NUM_STREAKS];
+CRGB blue = 0x000022;
+CRGB purple = 0x080010;
+Streak * streaks[NUM_STREAKS];
 Ladder * pinkL[NUM_STREAKS];
 Ladder * blueL[NUM_STREAKS];
 Ladder * purpleL[NUM_STREAKS];
+
+int active = 0;
 
 void setup() {
   Serial.begin(9600);
@@ -31,27 +36,31 @@ void setup() {
   FastLED.show();
   delay(2000);
 
-  // for(unsigned int i=0; i<NUM_STREAKS; i++) {
-  //   streaks[i] = new Streak(COLUMNS, ROWS, leds, pink);
-  // }
   for(unsigned int i=0; i<NUM_STREAKS; i++) {
+    streaks[i] = new Streak(COLUMNS, ROWS, leds, pink);
+  }
+
+  for(unsigned int i=0; i<NUM_LADDERS; i++) {
     pinkL[i] = new Ladder(COLUMNS, ROWS, leds, pink);
-    blueL[i] = new Ladder(COLUMNS, ROWS, leds, 0x000022);
-    purpleL[i] = new Ladder(COLUMNS, ROWS, leds, 0x080010);
+    blueL[i] = new Ladder(COLUMNS, ROWS, leds, blue);
+    purpleL[i] = new Ladder(COLUMNS, ROWS, leds, purple);
   }
 }
 
 void loop() {
     unsigned long currentTime = millis();
     clear();
-    // for(unsigned int i=0; i<NUM_STREAKS; i++) {
-    //   streaks[i]->display(currentTime);
-    // }
+
+    for(unsigned int i=0; i<NUM_STREAKS; i++) {
+      streaks[i]->display(currentTime);
+    }
+
     for(unsigned int i=0; i<NUM_STREAKS; i++) {
       pinkL[i]->display(currentTime);
       blueL[i]->display(currentTime);
       purpleL[i]->display(currentTime);
     }
+
     FastLED.show();
 }
 
