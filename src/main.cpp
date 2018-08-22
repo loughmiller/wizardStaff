eea1c942edfe65e4d353ae9000a0d71eef3f107f
Scott Loughmiller <loughmsa@gmail.com>
2018-08-22 01:35:40 -0700

    better power usage and brightness controls

--- src/main.cpp
+++ src/main.cpp
@@ -14,12 +14,12 @@
 #define ROWS 164
 #define COLUMNS 8
 #define NUM_LEDS ROWS*COLUMNS
-#define STEAL_COLOR_PIN 1
-#define CLEAR_COLOR_PIN 0
+#define CONTROL_UP 1
+#define CONTROL_DOWN 0
+#define CONTROL_MODE 23
 #define SENSOR_LED_PIN 16
 #define DISPLAY_LED_PIN 12
 #define BATTERY_PIN A7
-#define BRIGHTNESS_CONTROL 23
 #define AUDIO_INPUT_PIN A8        // Input pin for audio data.
 
 #define ANALOG_RATIO 310.3
@@ -27,8 +27,20 @@
 #define BATTERY_INTERCEPT -3.14
 #define BATTERY_APLPHA 0.2
 #define BATTERY_DEAD_READING 690
-#define BATTERY_READ_INTERVAL 60000
+#define BATTERY_READ_INTERVAL 120000
+#define BATTERY_LOAD_OFFSET 1.07
 
+#define MODE_COLOR_STEAL 0
+#define MODE_BRIGHTNESS 1
+#define MODE_SPECTRUM_COLOR 2
+#define MODE_SPECTRUM_TRAVEL 3
+#define MODE_SPARKLE_COLOR 4
+#define MODE_SPARKLE_AMOUNT 5
+#define MODE_STREAK_COLOR 6
+#define MODE_STREAK_COUNT 7
+#define MODES 2
+
+#define BRIGHTNESS 224
 #define SATURATION 244
 #define NUM_STREAKS 3
 
@@ -44,8 +56,21 @@ void defaultAllHues();
 void changeAllHues(uint8_t hue);
 void stealColorAnimation(uint8_t hue);
 void readAccelerometer();
-
 uint16_t xy2Pos(uint16_t x, uint16_t y);
+
+// ACTIONS
+void stealColor();
+void clearStolenColor();
+void increaseBrightness();
+void decreaseBrightness();
+void increaseSpectrumHue();
+void decreaseSpectrumHue();
+
+// GLOBALS (OMG - WTF?)
+uint_fast8_t currentMode = 0;
+uint_fast8_t currentBrightness = BRIGHTNESS;
+CHSV currentModeColor((256/MODES) * currentMode, SATURATION, 32);
+unsigned long buttonTimestamp = 0;
 
 uint16_t batteryReading = 2000;
 unsigned long batteryTimestamp = 0;
@@ -53,6 +78,8 @@ unsigned long batteryTimestamp = 0;
 uint8_t pinkHue = 240;
 uint8_t blueHue = 137;
 uint8_t greenHue = 55;
+
+uint_fast8_t currentSpectrumHue = blueHue;
 
 CHSV blueBatteryMeterColor(blueHue, SATURATION, 64);
 CRGB redBatteryMeeterColor = 0x060000;
@@ -84,8 +111,9 @@ void setup() {
 
   // SETUP LEDS
   FastLED.addLeds<NEOPIXEL, DISPLAY_LED_PIN>(leds, NUM_LEDS).setCorrection( 0xFFD08C );;
-  FastLED.setMaxPowerInVoltsAndMilliamps(5, 3000);
   FastLED.setDither(1);
+  FastLED.setMaxPowerInVoltsAndMilliamps(5, 2600);
+  FastLED.setBrightness(currentBrightness);
 
   // INDICATE BOOT SEQUENCE
   setAll(0x000200);
@@ -94,9 +122,9 @@ void setup() {
 
   // COLOR SENSOR
   pinMode(SENSOR_LED_PIN, OUTPUT);
-  pinMode(STEAL_COLOR_PIN, INPUT);
-  pinMode(CLEAR_COLOR_PIN, INPUT);
-  pinMode(BRIGHTNESS_CONTROL, INPUT);
+  pinMode(CONTROL_UP, INPUT);
+  pinMode(CONTROL_DOWN, INPUT);
+  pinMode(CONTROL_MODE, INPUT);
 
   // COLOR SENSOR SETUP
   if (tcs.begin()) {
@@ -134,50 +162,57 @@ void setup() {
   Serial.println("Sparkles!");
 
   spectrumTop = new Spectrum(COLUMNS, ROWS, (ROWS / 2) - 1, ROWS/2,
-    blueHue, SATURATION, true, 100, leds);
+    currentSpectrumHue, SATURATION, true, 100, leds);
   spectrumBottom = new Spectrum(COLUMNS, ROWS, ROWS / 2, ROWS/2,
-    blueHue, SATURATION, false, 100, leds);
+    currentSpectrumHue, SATURATION, false, 100, leds);
   spectrumTopFull = new Spectrum(COLUMNS, ROWS, 0, ROWS/2,
-    blueHue, SATURATION, false, 100, leds);
+    currentSpectrumHue, SATURATION, false, 100, leds);
   spectrumBottomFull = new Spectrum(COLUMNS, ROWS, ROWS-1, ROWS/2,
-    blueHue, SATURATION, true, 100, leds);
+    currentSpectrumHue, SATURATION, true, 100, leds);
 
   Serial.println("Spectrum Setup");
 
   Serial.println("setup complete");
 }
 
+// LOOP
 void loop() {
   clear();  // this just sets the array, no reason it can't be at the top
-
-
-  // Serial.println(touchRead(STEAL_COLOR_PIN));
-  // Serial.println(touchRead(CLEAR_COLOR_PIN));
-  // Serial.println(touchRead(BRIGHTNESS_CONTROL));
-
-  if (touchRead(STEAL_COLOR_PIN) > 4000) {
-    Serial.println("Read Color");
-    uint8_t hue = readHue();
-    Serial.println(hue);
-    stealColorAnimation(hue);
-    changeAllHues(hue);
-  }
-
-  if (touchRead(CLEAR_COLOR_PIN) > 4000) {
-    Serial.println("Clear Color");
-    defaultAllHues();
-  }
-
-  if (touchRead(BRIGHTNESS_CONTROL) > 4000) {
-    Serial.println("Change Brightness");
-  }
-
-  //  Serial.println(spectrumTop->getHue());
   unsigned long currentTime = millis();
+
+
+  // Serial.println(touchRead(CONTROL_UP));
+  // Serial.println(touchRead(CONTROL_DOWN));
+  // Serial.println(touchRead(CONTROL_MODE));
+
+  if (currentTime > buttonTimestamp + 1000) {
+    buttonTimestamp = currentTime;
+
+    if (touchRead(CONTROL_MODE) > 4000) {
+      currentMode = (currentMode + 1) % MODES;
+      currentModeColor.hue = (256/MODES) * currentMode;
+      Serial.print("Change Mode to ");
+      Serial.println(currentMode);
+    }
+
+    if (touchRead(CONTROL_UP) > 4000) {
+      switch(currentMode) {
+        case 0: stealColor();
+        case 1: increaseBrightness();
+      }
+    }
+
+    if (touchRead(CONTROL_DOWN) > 4000) {
+      switch(currentMode) {
+        case 0: clearStolenColor();
+        case 1: decreaseBrightness();
+      }
+    }
+  }
 
   // BATTERY READ
   if (currentTime > batteryTimestamp + BATTERY_READ_INTERVAL) {
-    float currentReading = analogRead(BATTERY_PIN);
+    float currentReading = analogRead(BATTERY_PIN) * BATTERY_LOAD_OFFSET;
 
     if (batteryReading == 2000) {
       batteryReading = (int)currentReading;
@@ -186,18 +221,18 @@ void loop() {
     }
 
     batteryTimestamp = currentTime;
-    Serial.print("raw: ");
-    Serial.println(currentReading);
-
-    Serial.print("moving average: ");
-    Serial.println(batteryReading);
+    Serial.print(batteryTimestamp);
+    Serial.print("\t");
+    Serial.print(currentReading);
+    Serial.print("\t");
+    Serial.print(batteryReading);
 
     float dividedVoltage = (float)batteryReading / ANALOG_RATIO;
-    Serial.print("dividedVoltage: ");
-    Serial.println(dividedVoltage);
+    Serial.print("\t");
+    Serial.print(dividedVoltage);
 
     float batteryPercentage = ((float)batteryReading * BATTERY_SLOPE) + BATTERY_INTERCEPT;
-    Serial.print("percentage: ");
+    Serial.print("\t");
     Serial.println(batteryPercentage);
 
     if (batteryReading < BATTERY_DEAD_READING) {
@@ -222,17 +257,17 @@ void loop() {
   }
 
   // BUTTON INDICATORS
-  // CHANGE BRIGHTNESS
-  leds[xy2Pos(1, 13)] = 0x000004;
-  leds[xy2Pos(2, 13)] = 0x000006;
+  // CHANGE MODE
+  leds[xy2Pos(1, 13)] = currentModeColor;
+  leds[xy2Pos(2, 13)] = currentModeColor;
 
   // CHANGE BRIGHTNESS
-  leds[xy2Pos(1, 21)] = 0x040000;
-  leds[xy2Pos(2, 21)] = 0x060000;
+  leds[xy2Pos(1, 21)] = 0x000400;
+  leds[xy2Pos(2, 21)] = 0x000400;
 
   // CHANGE BRIGHTNESS
-  leds[xy2Pos(1, 28)] = 0x000400;
-  leds[xy2Pos(2, 28)] = 0x000600;
+  leds[xy2Pos(1, 28)] = 0x040000;
+  leds[xy2Pos(2, 28)] = 0x040000;
 
   // MAIN DISPLAY
   // Serial.println();
@@ -250,6 +285,52 @@ void loop() {
   sparkle->display();
 
   FastLED.show();
+}
+// /LOOP
+
+// ACTIONS
+
+void stealColor() {
+  uint8_t hue = readHue();
+  Serial.println(hue);
+  stealColorAnimation(hue);
+  changeAllHues(hue);
+}
+
+void clearStolenColor() {
+  Serial.println("Clear Color");
+  defaultAllHues();
+}
+
+void increaseBrightness() {
+  if (currentBrightness != 240) {
+    currentBrightness = (currentBrightness + 16) % 256;
+    FastLED.setBrightness(currentBrightness);
+  }
+}
+
+void decreaseBrightness() {
+  if (currentBrightness != 0) {
+    currentBrightness = (currentBrightness - 16) % 256;
+    FastLED.setBrightness(currentBrightness);
+  }
+}
+
+void increaseSpectrumHue() {
+  if (currentSpectrumHue != 255) {
+    currentSpectrumHue = (currentSpectrumHue + 5) % 256;
+    spectrumTop->setHue(currentSpectrumHue);
+    spectrumBottom->setHue(currentSpectrumHue);
+    spectrumTopFull->setHue(currentSpectrumHue);
+    spectrumBottomFull->setHue(currentSpectrumHue);
+  }
+}
+
+void decreaseSpectrumHue() {
+  if (currentSpectrumHue != 0) {
+    currentSpectrumHue = (currentSpectrumHue - 5) % 256;
+    FastLED.setBrightness(currentBrightness);
+  }
 }
 
 void setAll(CRGB color) {
@@ -283,13 +364,13 @@ void defaultAllHues() {
     streaks[i]->setRandomHue(true);
   }
 
-  spectrumTop->setHue(blueHue);
+  spectrumTop->setHue(currentSpectrumHue);
   spectrumTop->setTravel(100);
-  spectrumBottom->setHue(blueHue);
+  spectrumBottom->setHue(currentSpectrumHue);
   spectrumBottom->setTravel(100);
-  spectrumTopFull->setHue(blueHue);
+  spectrumTopFull->setHue(currentSpectrumHue);
   spectrumTopFull->setTravel(100);
-  spectrumBottomFull->setHue(blueHue);
+  spectrumBottomFull->setHue(currentSpectrumHue);
   spectrumBottomFull->setTravel(100);
 }
 
@@ -338,16 +419,19 @@ void stealColorAnimation(uint8_t hue) {
   CRGB color = CHSV(hue, SATURATION, 255);
   setAll(off);
 
+  FastLED.setBrightness(64);
+
   float d = 125;
   for (uint16_t y=0; y<ROWS; y++) {
     for (uint8_t x=0; x<COLUMNS; x++) {
       leds[xy2Pos(x, y)] = color;
     }
     FastLED.show();
-    delay((int)(d *= 0.8));
+    // FastLED.delay((int)(d *= 0.8));
   }
 
   // Serial.println("StealColorAnimation Complete.");
+  FastLED.setBrightness(currentBrightness);
 }
 
 
